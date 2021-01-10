82b91ac83be85e8434a79a2b68fd79e22f900cda
naveenkosigi <naveenkosigi@gmail.com>
2021-01-10 17:16:32 +0530

    Added dynamic form addition for adding ingredients

--- src/app/edit-recipe/edit-recipe.component.ts
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -1,9 +1,10 @@
+import { ingredient } from './../models/ingredient.model';
 import { recipe } from './../models/recipe.model';
 import { recipeService } from './../Services/recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { FormControl, FormGroup} from '@angular/forms';
+import { Form, FormArray, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-recipe',
@@ -30,20 +31,40 @@ export class EditRecipeComponent implements OnInit {
     let name:String="";
     let imagePath:String="";
     let description:String="";
+    let ingredients;
+    let formArray=new FormArray([]);
 
     if(this.editMode){
-      ({name,imagePath,description}=this.recipeService.getRecipeByIndex(this.id-1));
+      ({name,imagePath,description,ingredients}=this.recipeService.getRecipeByIndex(this.id-1));
+      if(ingredients){
+        for(const ing of ingredients){
+          formArray.push(new FormGroup({
+            'ingredient':new FormControl(ing.name),
+            'amount':new FormControl(ing.cost)
+          }));
+        }
+      }
     }
 
     this.form=new FormGroup({
       'name':new FormControl(name),
       'imagePath':new FormControl(imagePath),
-      'description':new FormControl(description)
+      'description':new FormControl(description),
+      'ingredients':formArray
     })
   }
 
   submitForm():void{
     console.log(this.form);
+  }
+
+  addIngredient():void{
+    (<FormArray>this.form.get('ingredients')['controls']).push(
+      new FormGroup({
+        "ingredient":new FormControl(),
+        "amount":new FormControl()
+      })
+    );
   }
 
 }
